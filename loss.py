f71584cf4560c9fa6db9362ecb5faf081d7b831e
NguyenMinhMY <nmmy.toan.cr@gmail.com>
2023-10-05 22:44:09 +0700

    add margin + ignore sigmoid func

--- loss.py
+++ loss.py
@@ -4,10 +4,11 @@ import math
 
 class ContrastiveLoss(nn.Module):
 
-    def __init__(self, margin=0.0):
+    def __init__(self, margin=math.cos(math.pi/5)):
         super(ContrastiveLoss, self).__init__()
 
-        self.margin = margin
+        # self.margin = margin
+        self.margin = math.cos(math.pi/5)
 
 
     def forward(self, anchors, pos_outs, neg_outs):
@@ -23,18 +24,20 @@ class ContrastiveLoss(nn.Module):
 
         # return loss
 
-        sigmoid = torch.nn.Sigmoid()
+        # sigmoid = torch.nn.Sigmoid()
 
         pos_centroids = torch.stack([self.get_centroids(embs) for embs in pos_outs], dim=0) # (B,D)
         neg_outs = torch.stack(neg_outs, dim=0) # (B,4,D)
 
         S_pos = self.cacl_similarity(anchors, pos_centroids, dim=1) # (B,1)
-        S_pos = sigmoid(S_pos)
+        # S_pos = sigmoid(S_pos)
+        loss_sim = 1 - S_pos
 
         S_negs = self.cacl_similarity(anchors.unsqueeze(1), neg_outs, dim=2) # (B,4,1)
-        S_negs = sigmoid(S_negs)
+        # S_negs = sigmoid(S_negs)
+        loss_dissims = torch.clamp(S_negs - self.margin, min=0.0)
 
-        loss = 1 - S_pos + torch.max(S_negs, dim=1).values
+        loss = loss_sim + torch.max(loss_dissims, dim=1).values
         return torch.sum(loss)/anchors.size()[0]
 
     def get_centroids(self, embeddings):
