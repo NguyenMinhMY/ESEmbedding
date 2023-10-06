1acdc54f585deb28adedb887ef383e2a8547be66
NguyenMinhMY <nmmy.toan.cr@gmail.com>
2023-10-06 22:56:13 +0700

    log loss

--- loss.py
+++ loss.py
@@ -10,39 +10,56 @@ class ContrastiveLoss(nn.Module):
         # self.margin = margin
         self.margin = math.cos(math.pi/5)
 
-        self.w = torch.nn.Parameter(torch.abs(torch.randn(1,)))
-        self.b = torch.nn.Parameter(torch.randn(1,))
+        # self.w = torch.nn.Parameter(torch.abs(torch.randn(1,)))
+        # self.b = torch.nn.Parameter(torch.randn(1,))
 
 
     def forward(self, anchors, pos_outs, neg_outs):
-        # cosine similarity
+        '''
+        1-Pair Loss - Cosine
 
-        # cosine = cos_func(x0, x1)
+        cos_func = torch.nn.CosineSimilarity(dim=1)
+        cosine = cos_func(x0, x1)
 
-        # loss_similarity = 1 - cosine
-        # loss_dissimlarity = torch.clamp(cosine - self.margin, min=0.0)
+        loss_similarity = 1 - cosine
+        loss_dissimlarity = torch.clamp(cosine - self.margin, min=0.0)
 
-        # loss = (1 - y)*loss_similarity + y*loss_dissimlarity
-        # loss = torch.sum(loss) / x0.size()[0]
+        loss = (1 - y)*loss_similarity + y*loss_dissimlarity
+        loss = torch.sum(loss) / x0.size()[0]
 
-        # return loss
+        return loss
+        '''
 
-        # sigmoid = torch.nn.Sigmoid()
+        '''
+        N-pair loss - Cosine + margin
 
         pos_centroids = torch.stack([self.get_centroids(embs) for embs in pos_outs], dim=0) # (B,D)
         neg_outs = torch.stack(neg_outs, dim=0) # (B,4,D)
 
         S_pos = self.cacl_similarity(anchors, pos_centroids, dim=1) # (B,1)
-        # S_pos = sigmoid(S_pos)
         loss_sim = 1 - S_pos
 
         S_negs = self.cacl_similarity(anchors.unsqueeze(1), neg_outs, dim=2) # (B,4,1)
-        # S_negs = sigmoid(S_negs)
         margin = self.w * self.margin + self.b
         loss_dissims = torch.clamp(S_negs - margin, min=0.0)
 
         loss = loss_sim + torch.max(loss_dissims, dim=1).values
         return torch.sum(loss)/anchors.size()[0]
+        '''
+
+        # N-pair loss - Cosine + logarit
+        pos_centroids = torch.stack([self.get_centroids(embs) for embs in pos_outs], dim=0) # (B,D)
+        neg_outs = torch.stack(neg_outs, dim=0) # (B,4,D)
+
+        S_pos = self.cacl_similarity(anchors, pos_centroids, dim=1) # (B,1)
+        S_negs = self.cacl_similarity(anchors.unsqueeze(1), neg_outs, dim=2) # (B,4,1)
+
+        S_pos = torch.exp(S_pos) # (B,1)
+        S_negs = torch.sum(torch.exp(S_negs), dim=1) # (B,1)
+
+        loss = - torch.log(S_pos / (S_pos + S_negs))
+        loss = torch.sum(loss)/anchors.size()[0]
+        return loss
 
     def get_centroids(self, embeddings):
         return torch.mean(embeddings, dim=0)
